6db0fe2e3743193063e50b0ca9005c0d3c87da5c
Teemu <teemu@example.com>
2025-12-19 13:56:31 +0200

    test: add teammate identification test cases

--- tests/test_court_movement.py
+++ tests/test_court_movement.py
@@ -7,3 +7,31 @@ def test_get_previous_teammates_empty_round():
     previous_matches = []
     result = get_previous_teammates(player_id=1, previous_matches=previous_matches)
     assert result == set()
+
+def test_get_previous_teammates_identifies_team1_partner():
+    """Test identifying teammate from team 1"""
+    previous_matches = [
+        {
+            'player1_id': 1,
+            'player2_id': 2,
+            'player3_id': 3,
+            'player4_id': 4,
+            'winning_team': 1
+        }
+    ]
+    result = get_previous_teammates(player_id=1, previous_matches=previous_matches)
+    assert result == {2}
+
+def test_get_previous_teammates_identifies_team2_partner():
+    """Test identifying teammate from team 2"""
+    previous_matches = [
+        {
+            'player1_id': 1,
+            'player2_id': 2,
+            'player3_id': 3,
+            'player4_id': 4,
+            'winning_team': 2
+        }
+    ]
+    result = get_previous_teammates(player_id=3, previous_matches=previous_matches)
+    assert result == {4}
